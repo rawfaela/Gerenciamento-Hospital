73f4b48b48a877a2ddf4980cecae9b0da69fe7ec
rafa <168464894+rawfaela@users.noreply.github.com>
2024-11-26 16:49:05 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,48 +1,47 @@
---MUDAR TODOS OS MÁXIMOS PRA 500
-
 create DATABASE hospital;
 
 create table medicos(
-    crm int(5)  not null,
-    nome varchar(50)  not null,
-    telefone int(8) not null,
-    endereco varchar(50) not null,
-    cidade varchar(50) not null,
-    estado char(2) not null,
-    especialidade varchar(50) not null,
+    crm                 int(5)         not null,
+    nome                varchar(500)   not null,
+    telefone            int(11)        not null,
+    endereco            varchar(500)   not null,
+    cidade              varchar(500)   not null,
+    estado              char(2)        not null,
+    especialidade       varchar(500)   not null,
     primary key (crm)
 );
 
+
 create table pacientes(
-    cpf int(11) not null,
-    nome varchar(50) not null,
-    telefone int(8) not null,
-    endereco varchar(50) not null,
-    cidade varchar(50) not null,
-    estado char(2) not null,
-    sexo char(1) not null,
-    datanasc date  not null,
-    estadocivil varchar(50) not null,
+    cpf                 int(11)        not null,
+    nome                varchar(500)   not null,
+    telefone            int(11)        not null,
+    endereco            varchar(500)   not null,
+    cidade              varchar(500)   not null,
+    estado              char(2)        not null,
+    sexo                char(1)        not null,
+    datanasc            date           not null,
+    estadocivil         varchar(500)   not null,
     primary key (cpf)
 );
 
 create table exames(
-    codigo int(5) not null,
-    descricao varchar(100) not null,
-    tipo varchar(50) not null,
-    preparo varchar(150),
-    pos_exame varchar(150),
+    codigo              int(5)         not null,
+    descricao           varchar(500)   not null,
+    tipo                varchar(500)   not null,
+    preparo             varchar(500),
+    pos_exame           varchar(500),
     primary key (codigo)
 );
 
 create table consultas(
-    codigo int(5) not null,
-    data date not null,
-    hora time  not null,
-    tipo varchar(50) not null,
-    codpaciente int(5) not null,
-    codmedico int(5) not null,
-    codexame int(5) not null,
+    codigo              int(5)         not null,
+    tipo                varchar(500)   not null,
+    data                date           not null,
+    hora                time           not null,
+    codpaciente         int(5)         not null,
+    codmedico           int(5)         not null,
+    codexame            int(5)         not null,
     primary key (codigo),
     foreign key (codpaciente) references pacientes(cpf),
     foreign key (codmedico) references medicos(crm),
