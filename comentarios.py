0c9294dd089ca3c588a176c57cee1abcbe37cde3
rafa <168464894+rawfaela@users.noreply.github.com>
2024-11-26 22:18:33 -0300

    Update comentarios.py

--- comentarios.py
+++ comentarios.py
@@ -64,7 +64,7 @@ def limitar_entrada(valor, campo): # valor: vai ser validado, campo: condicao
         return False
 
     elif campo == 'sexo':
-        if valor.isalpha() and valor == 1:
+        if valor.isalpha() and len(valor) == 1:
             return True
         return False
 
